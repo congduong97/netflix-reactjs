fcad12fbb0e5d1690275f2d4ad795749e72437c3
cong.dv <duongcongbka@gmail.com>
2023-06-25 22:43:42 +0700

    iframe

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    window.addEventListener('message', (event) => {
+      console.log('postMessage',event);
+      
+    })
+  
+    return () => {
+      window.removeEventListener('message',()=>{})
+    }
+  }, [])
+  
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div style={{width:'100vw', height:'100vh'}}>
+        <iframe
+          height={'500px'}
+          width={'500px'}
+          src="https://netflix-nextjs-rouge.vercel.app/home"
+          title="W3Schools Free Online Web Tutorials"
+        ></iframe>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
